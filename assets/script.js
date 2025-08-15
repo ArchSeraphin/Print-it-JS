38323b9ff7257015643396547d6a54f97938abbd
ArchSeraphin <trinity.informatique@gmail.com>
2025-08-15 11:19:40 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,10 @@ const slides = [
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	}
 ]
 
